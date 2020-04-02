59a0c01dbdb561e425862e3f1ba289073eba18c0
IssamAmajoud <45203821+IssamAmajoud@users.noreply.github.com>
2020-04-02 13:18:37 +0200

    Add files via upload

---
+++ Groet.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>scale OOP</title>
+</head>
+<body>
+	<h1> Schrijf "Hallo computer" </h1>
+    <form method="post">
+    	<input type="text" name="cijfer_1">
+    	<input type="submit" name="submit">
+    </form>
+</body>
+</html>
+<?php
+
+
+if(isset($_POST['submit'])) {
+	
+	$reactie = $_POST['cijfer_1'];
+
+
+	if ($reactie == "Hallo computer") 
+	{
+    	echo "Hallo gebruiker!";
+	}
+	if ($reactie == "hallo computer") 
+	{
+    	echo "Hallo gebruiker!";
+	}
+	
+}
+
+?>
